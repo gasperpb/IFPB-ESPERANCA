38c6aaf1d672d99be035e0f02161737cacfe1c13
gasperpb <you@example.com>
2022-03-16 19:02:09 -0300

    telas

---
+++ app_gym/appGym/MOCK/Avatar.js
@@ -0,0 +1,9 @@
+export default [
+  { name: "Achilles", image: require("../assets/avatar_1.jpg"), id: 1 },
+  { name: "Vasconcelos", image: require("../assets/Avatar_2.jpeg"), id: 2 },
+  { name: "João", image: require("../assets/Avatar_3.jpg"), id: 3 },
+  { name: "Budi", image: require("../assets/Avatar_2.png"), id: 4 },
+  { name: "Johny", image: require("../assets/avatar_1.jpg"), id: 5 },
+  { name: "Andy", image: require("../assets/Avatar_2.jpeg"), id: 6 },
+  { name: "Raphael", image: require("../assets/Avatar_3.jpg"), id: 7 }
+];
---
+++ app_gym/appGym/MOCK/categories.js
@@ -0,0 +1,8 @@
+export default [
+  { name: "Iniciante", image: require("../assets/capa.jpg"), id: 1 },
+  { name: "Sob Demanda", image: require("../assets/secondaryCard_2.png"), id: 2 },
+  { name: "Trainers", image: require("../assets/capa.jpg"), id: 3 },
+  { name: "Trending", image: require("../assets/secondaryCard_2.png"), id: 4 },
+  { name: "Trainers", image: require("../assets/capa.jpg"), id: 5 },
+  { name: "Zumba", image: require("../assets/secondaryCard_2.png"), id: 6 },
+];
